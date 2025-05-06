5736d2b64958bbf0ca380ae13be5f2d7d022cb9c
Giorgi Koridze <123615886+GioKori@users.noreply.github.com>
2025-05-06 15:41:36 +0300

    Add files via upload

---
+++ QA-TLNN/backTobasics.js
@@ -0,0 +1,19 @@
+let age = 3;
+let firstLetter = "G";
+let score = 4;
+let initial = "initial word"
+
+console.log(age);
+console.log(firstLetter);
+console.log(score);
+console.log(initial);
+
+age = 22;
+firstLetter = "T";
+score = 77;
+intial = "redeclare";
+
+console.log(intial);
+console.log(firstLetter);
+console.log(age);
+console.log(score);
